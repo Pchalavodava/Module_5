6047e90e018c25a14424ca7fcb13db23fa695701
Pchelovodova <pchelovodovaes@gmail.com>
2025-02-09 14:03:06 +0100

    Практикум 1. Работа с файлами. Работа над ошибками

--- practice_1.py
+++ practice_1.py
@@ -1,8 +1,16 @@
 import json
 import csv
 from datetime import datetime
+from typing import Union
 
 from cprint import cprint
+
+
+def read_csv(csv_file):
+    file = open(csv_file, 'r', encoding='utf-8')
+    reader = csv.reader(file)
+    return reader
+
 
 """Задание 1: Работа с JSON файлом
 Задача:
@@ -15,18 +23,18 @@ from cprint import cprint
 Определить количество студентов, изучающих определенный предмет (например, Python)."""
 
 
-def get_list_from_json(file: str) -> list[dict[str, str | int]]:
+def get_list_from_json(file: str) -> list[dict[str, Union[str, int]]]:
     """
     Конвертация файла json в объект Python (список)
     :param file: str: Файл json для обработки
     :return: list[dict[str, str | int]]: Список словарей (студентов)
     """
     with open(file, 'r', encoding='utf-8') as json_file:
-        list_from_json: list[dict[str, str | int]] = json.load(json_file)
+        list_from_json: list[dict[str, Union[str, int]]] = json.load(json_file)
         return list_from_json
 
 
-def get_numbers_of_students(students: list[dict[str, str | int]]) -> int:
+def get_numbers_of_students(students: list[dict[str, Union[str, int]]]) -> int:
     """
     Получение общего количества студентов (общее количество элементов в списке)
     :param students: list[dict[str, str | int]]: Список студентов
@@ -35,19 +43,19 @@ def get_numbers_of_students(students: list[dict[str, str | int]]) -> int:
     return len(students)
 
 
-def get_max_age_student(students: list[dict[str, str | int]]) -> tuple[str, int, str]:
+def get_max_age_student(students: list[dict[str, Union[str, int]]]) -> tuple[str, int, str]:
     """
     Получение самого старшего студента
     :param students: list[dict[str, str | dict]]: Список студентов для поиска старшего из них
     :return: tuple[str, int, str]: Кортеж из имени, возраста и города старшего студента
     """
     max_age: int = max(student.get('возраст') for student in students)
-    older_student: list[dict[str, str | int]] = [student for student in students if student.get('возраст') == max_age]
+    older_student: list[dict[str, Union[str, int]]] = [student for student in students if student.get('возраст') == max_age]
     for max_age_student in older_student:
         return max_age_student.get('имя'), max_age_student.get('возраст'), max_age_student.get('город')
 
 
-def get_students_by_discipline(students: list[dict[str, str | int]]) -> dict[str, int]:
+def get_students_by_discipline(students: list[dict[str, Union[str, int]]]) -> dict[str, int]:
     """
     Получение словаря предметов, где ключ - это название предмета, значение - количество студентов,
     изучающих данный предмет
@@ -71,7 +79,7 @@ def file_analysis(file: str) -> None:
     :param file: str: json-файл для обработки
     :return: None
     """
-    list_of_students: list[dict[str, str | int]] = get_list_from_json(file)
+    list_of_students: list[dict[str, Union[str, int]]] = get_list_from_json(file)
     total_students: int = get_numbers_of_students(list_of_students)
     older_student: tuple[str, int, str] = get_max_age_student(list_of_students)
     students_by_disciplines: dict[str, int] = get_students_by_discipline(list_of_students)
@@ -94,24 +102,25 @@ file_analysis('students.json')
 Разделить данные на категории по месяцам и вывести общую сумму продаж для каждого месяца."""
 
 
-def get_goods_list(csv_file: str) -> list[dict[str, str | int]]:
+def get_goods_list(csv_file: str) -> list[dict[str, Union[str, int]]]:
     """
     Формирование списка словарей товаров из csv-файла
     :param csv_file: str: csv-файл для обработки
     :return: list[dict[str, str | int]]: Список словарей (товаров)
     """
-    goods_list: list[dict[str, str | int]] = []
-    with open(csv_file, 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        reader = list(map(lambda y: list(map(lambda x: x.strip(), y)), reader))
-        i: int = 1
-        while i < len(reader):
-            goods_list.append(dict(zip(reader[0], reader[i])))
-            i += 1
+    goods_list: list[dict[str, Union[str, int]]] = []
+    # with open(csv_file, 'r', encoding='utf-8') as file:
+    #     reader = csv.reader(file)
+    reader = read_csv(csv_file)
+    reader = list(map(lambda y: list(map(lambda x: x.strip(), y)), reader))
+    i: int = 1
+    while i < len(reader):
+        goods_list.append(dict(zip(reader[0], reader[i])))
+        i += 1
     return goods_list
 
 
-def get_total_sales_amount(sales: list[dict[str, str | int]]) -> int:
+def get_total_sales_amount(sales: list[dict[str, Union[str, int]]]) -> int:
     """
     Расчет общей суммы продаж по всем товарам
     :param sales: list[dict[str, str | int]]: Список товаров
@@ -123,7 +132,7 @@ def get_total_sales_amount(sales: list[dict[str, str | int]]) -> int:
     return total
 
 
-def get_most_popular_product(sales: list[dict[str, str | int]]) -> tuple[str, int]:
+def get_most_popular_product(sales: list[dict[str, Union[str, int]]]) -> tuple[str, int]:
     """
     Поиск продукта с наибольшим объемом продаж
     :param sales: list[dict[str, str | int]]: Список товаров
@@ -140,7 +149,7 @@ def get_most_popular_product(sales: list[dict[str, str | int]]) -> tuple[str, in
     return most_popular_product, goods_dict[most_popular_product]
 
 
-def sales_by_month(sales: list[dict[str, str | datetime]]) -> dict[str, int]:
+def sales_by_month(sales: list[dict[str, Union[str, datetime]]]) -> dict[str, int]:
     """
     Формирование словаря продаж по месяцам
     :param sales: list[dict[str, str | datetime]]: Список товаров для обработки
@@ -163,7 +172,7 @@ def file_analysis(file: str) -> None:
     :param file: str: Входящий csv-файл для обработки
     :return: None
     """
-    goods_list: list[dict[str, str | int]] = get_goods_list(file)
+    goods_list: list[dict[str, Union[str, int]]] = get_goods_list(file)
     total_sales: int = get_total_sales_amount(goods_list)
     most_popular_product: tuple[str, int] = get_most_popular_product(goods_list)
     month_sales: dict[str, int] = sales_by_month(goods_list)
@@ -186,36 +195,37 @@ file_analysis('sales.csv')
 Найти сотрудника с наивысшей производительностью и вывести его имя и показатель производительности."""
 
 
-def get_employees_list(json_file: str) -> list[dict[str, str | int]]:
+def get_employees_list(json_file: str) -> list[dict[str, Union[str, int]]]:
     """
     Конвертация файла json в список сотрудников
     :param json_file: str: json-файл для обработки
     :return: list[dict[str, str | int]]
     """
     with open(json_file, 'r', encoding='utf-8') as file:
-        data: list[dict[str, str | int]] = json.load(file)
+        data: list[dict[str, Union[str, int]]] = json.load(file)
     return data
 
 
-def get_performance_list(csv_file: str) -> list[dict[str, str | int]]:
+def get_performance_list(csv_file: str) -> list[dict[str, Union[str, int]]]:
     """
     Получение списка производительностей по каждому сотруднику
     :param csv_file: str: csv_файл для обработки
     :return: list[dict[str, str | dict]]
     """
-    performance_lists: list[dict[str, str | int]] = []
-    with open(csv_file, 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        reader = list(map(lambda y: list(map(lambda x: x.strip(), y)), reader))
-        i = 1
-        while i < len(reader):
-            performance_lists.append(dict(zip(reader[0], reader[i])))
-            i += 1
+    performance_lists: list[dict[str, Union[str, int]]] = []
+    # with open(csv_file, 'r', encoding='utf-8') as file:
+    #     reader = csv.reader(file)
+    reader = read_csv(csv_file)
+    reader = list(map(lambda y: list(map(lambda x: x.strip(), y)), reader))
+    i = 1
+    while i < len(reader):
+        performance_lists.append(dict(zip(reader[0], reader[i])))
+        i += 1
     return performance_lists
 
 
-def get_employees_performance(employees_list: list[dict[str, str | int]],
-                              performance_list: list[dict[str, str | int]]) -> list[dict[str, str | int]]:
+def get_employees_performance(employees_list: list[dict[str, Union[str, int]]],
+                              performance_list: list[dict[str, Union[str, int]]]) -> list[dict[str, Union[str, int]]]:
     """
     Получение списка сотрудников с добавлением производительности посредством слияния словарей по
     id-номеру сотрудника
@@ -230,7 +240,7 @@ def get_employees_performance(employees_list: list[dict[str, str | int]],
     return employees_list
 
 
-def get_average_performance(employees: list[dict[str, str | int]]) -> float:
+def get_average_performance(employees: list[dict[str, Union[str, int]]]) -> float:
     """
     Определение средней производительности всех сотрудников
     :param employees: list[dict[str, str | int]]: Список сотрудников для обработки
@@ -244,14 +254,14 @@ def get_average_performance(employees: list[dict[str, str | int]]) -> float:
         print('В списке нет ни одного сотрудника')
 
 
-def get_max_performance(employees: list[dict[str, str | int]]) -> tuple[str, int]:
+def get_max_performance(employees: list[dict[str, Union[str, int]]]) -> tuple[str, int]:
     """
     Получение сотрудника с наибольшей производительностью
     :param employees: list[dict[str, str | int]]: Список сотрудников
     :return: str: Имя наиболее производительного сотрудника
     """
     max_performance: int = max(employee.get('производительность') for employee in employees)
-    the_best_employee: list[dict[str, str | int]] = [employee for employee in employees if
+    the_best_employee: list[dict[str, Union[str, int]]] = [employee for employee in employees if
                                                      employee['производительность'] == max_performance]
     for name in the_best_employee:
         return name.get('имя'), name.get('производительность')
@@ -264,9 +274,9 @@ def performance_analysis(json_file: str, csv_file: str) -> None:
     :param csv_file: Входящий csv-файл со списком производительностей сотрудников
     :return: None
     """
-    employees: list[dict[str, str | int]] = get_employees_list(json_file)
-    performance: list[dict[str, str | int]] = get_performance_list(csv_file)
-    employees_performance: list[dict[str, str | int]] = get_employees_performance(employees, performance)
+    employees: list[dict[str, Union[str, int]]] = get_employees_list(json_file)
+    performance: list[dict[str, Union[str, int]]] = get_performance_list(csv_file)
+    employees_performance: list[dict[str, Union[str, int]]] = get_employees_performance(employees, performance)
     average_performance: float = get_average_performance(employees_performance)
     the_best_employee: tuple[str, int] = get_max_performance(employees_performance)
     cprint(f'Средняя производительность всех сотрудников составляет {average_performance}\n'
